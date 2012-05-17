f71dbbd27ff3b92157579c8f4848e42b65cdbffd
Tomas Frydrych <tomas@sleepfive.com>
2012-05-17 20:28:35 +0100

    Add xdg autostart session script; rm PA and MEX from the rygel script

--- meta-guacamayo/recipes-core/guacamayo-session/guacamayo-session-x11/etc/X11/Xsession.d/90rygel.sh
+++ meta-guacamayo/recipes-core/guacamayo-session/guacamayo-session-x11/etc/X11/Xsession.d/90rygel.sh
@@ -1,14 +1,2 @@
-# FIXME -- this is obviously just a place holder, but both of these need to
-# exit with session, which the currently do not
-echo "Starting Pulse Audio server"
-/usr/bin/pulseaudio -D --exit-idle-time=-1
-
-# When we get this properly sorted, we will make MEX to be the last execed
-# but for now put it here, so that when it does not work, we still get
-# X session running
-echo "Starting MEX"
-media-explorer -f &
-
-echo "Starting Rygel"
 exec /usr/bin/rygel
 
