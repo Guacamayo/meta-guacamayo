ae64d267eda1bc3bf948f27ba5cef8438b62621e
Tomas Frydrych <tomas@sleepfive.com>
2012-05-17 10:40:00 +0100

    guacamayo-session-x11: fix rygel session script
    Start the PA server.

--- meta-guacamayo/recipes-core/guacamayo-session/guacamayo-session-x11/etc/X11/Xsession.d/90rygel.sh
+++ meta-guacamayo/recipes-core/guacamayo-session/guacamayo-session-x11/etc/X11/Xsession.d/90rygel.sh
@@ -1,1 +1,7 @@
+# FIXME -- this is obviously just a place holder, but both of these need to
+# exit with session, which the currently do not
+echo "Starting Pulse Audio server"
+/usr/bin/pulseaudio -D --exit-idle-time=-1
+
+echo "Starting Rygel"
 exec /usr/bin/rygel
